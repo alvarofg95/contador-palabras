04372f4ec66cd2f08e8bf677faf5abf8b7f46aaf
Alvaro Fernandez <alvarofg95dev@gmail.com>
2019-05-31 10:38:32 +0200

    contact form deleted

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { Route, withRouter, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import './index.css';
-import Contacto from './pages/Contacto';
 
 function App() {
   return (
     <div className="App">
       <header>
         <Link to="/">Home</Link>
-        <Link to="/contacto">Contacto</Link>
       </header>
       <Route exact path="/" component={Home} />
-      <Route exact path="/contacto" component={Contacto} />
     </div>
   );
 }
